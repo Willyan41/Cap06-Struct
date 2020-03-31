2bd6d19bf60045434bce71cc4e5d1e8c6050ebee
Willyan Santos <willyan41@hotmail.com>
2020-03-31 16:56:34 -0300

    Adicionar arquivos de projeto.

---
+++ Cap06 Struct/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap06_Struct
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Produto(String nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+
+        }
+
+
+    }
+}
---
+++ Cap06 Struct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap06_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de produtos.");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("Digite o nome do produto");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Digite o valor do produto");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Produto(nome, valor);
+
+            }
+            double soma = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                soma += vect[i].Valor;
+
+            }
+            double media = soma / n;
+
+            Console.WriteLine("Preço médio R$ {0}", media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
